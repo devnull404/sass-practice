fe49f21ebb0721a658930768efbcb5058a60110e
Francisco <ecoli404@icloud.com>
2020-11-30 00:48:09 -0600

    Finishing interactive nav

--- javascript/interactive.js
+++ javascript/interactive.js
@@ -1,9 +1,4 @@
-window.addEventListener("resize", function(e) {
-    if (window.innerWidth < 1100) {
-        var color = 254 -  (window.innerWidth/1100)*80;
-        document.getElementsByTagName("nav")[0].style.backgroundColor = "rgb(157, 123, " + String(color) + ")"
-    }
-    if (window.innerWidth >= 1100) {
-        document.getElementsByTagName("nav")[0].style.backgroundColor = "rgb(157, 123, 152)"
-    }
+window.addEventListener("scroll", function(e) {
+    var color = 254 -  (window.scrollY/(document.body.scrollHeight-window.screen.height))*254;
+    document.getElementsByTagName("nav")[0].style.backgroundColor = "rgb(157, 123, " + String(color) + ")"
 });
