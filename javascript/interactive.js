c22a2dbe314c2d3232b68e3c91b0f489eeebb61c
Francisco <ecoli404@icloud.com>
2020-11-29 19:54:53 -0600

    Fixing nav

--- javascript/interactive.js
+++ javascript/interactive.js
@@ -1,6 +1,6 @@
 window.addEventListener("resize", function(e) {
     if (window.innerWidth < 800) {
-        var color = 253 -  (window.innerWidth/800)*100;
+        var color = 254 -  (window.innerWidth/800)*80;
         document.getElementsByTagName("nav")[0].style.backgroundColor = "rgb(157, 123, " + String(color) + ")"
     }
 });
