d44d6f6014030d477e0f0c587d417018e9550e1d
Francisco <ecoli404@icloud.com>
2020-11-30 00:20:59 -0600

    adding content

--- javascript/interactive.js
+++ javascript/interactive.js
@@ -1,6 +1,9 @@
 window.addEventListener("resize", function(e) {
-    if (window.innerWidth < 800) {
-        var color = 254 -  (window.innerWidth/800)*80;
+    if (window.innerWidth < 1100) {
+        var color = 254 -  (window.innerWidth/1100)*80;
         document.getElementsByTagName("nav")[0].style.backgroundColor = "rgb(157, 123, " + String(color) + ")"
+    }
+    if (window.innerWidth >= 1100) {
+        document.getElementsByTagName("nav")[0].style.backgroundColor = "rgb(157, 123, 152)"
     }
 });
